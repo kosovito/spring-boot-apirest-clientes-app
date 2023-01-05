566685037b85141f590671c973d59cbcd79f58c5
kosovito <kosovito@gmail.com>
2023-01-05 10:51:58 +0100

    initial commit

---
+++ src/main/java/com/bolsadeideas/springboot/apirest/clientes/app/SpringBootApirestClientesAppApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.apirest.clientes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApirestClientesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApirestClientesAppApplication.class, args);
+	}
+
+}
